23153cab58b85b90ac13f56c49830899d3bd20f0
Raisa Guayasamin <raisa.marilyn1993@gmail.com>
2024-05-20 12:40:01 -0500

    Creacion de formularios reactivos basico, dinamico, registro y validaciones

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
+
+@Component({
+  selector: 'auth-register-page',
+  templateUrl: './register-page.component.html',
+  styles: ``,
+})
+export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(customValidators.firstNameAndLastnamePattern),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+        [this.emailValidatorService],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorsService.cantBeRaisicela],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2'),
+      ],
+    }
+  );
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidatorService: EmailValidatorService
+  ) {}
+
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched;
+  }
+}
